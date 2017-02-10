e254289b8e616283c58e5b38f50bed5a725d5c25
transybao93 <IT_coder_93@yahoo.com.vn>
2017-02-10 18:56:42 +0700

    Delete img folder, change public folder structure, add index.blade.php page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,130 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Catatan Si Boy a Bootstarp  Website Template | Home :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,800,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+<script src="js/jquery.min.js"></script>				
+</head>
+<body>
+<!-- header -->
+	<div class="banner">
+  	    <div class="container">
+			<div class="logo">
+				<a href="index.html"><img src="images/logo.png" class="img-responsive" alt="" /></a>
+			</div>
+		</div> 
+			<div class="header-bottom">
+				<div class="container">
+					<div class="head-nav">
+						<span class="menu"> </span>
+							<ul>
+								<li class="active"><a href="index.html">Home</a></li>
+								<li><a href="about.html">About</a></li>
+								<li><a href="index.html">Categories</a></li>
+								<div class="clearfix"> </div>
+						</ul>
+				</div>
+				<!-- script-for-nav -->
+					<script>
+						$( "span.menu" ).click(function() {
+						  $( ".head-nav ul" ).slideToggle(300, function() {
+							// Animation complete.
+						  });
+						});
+					</script>
+				<!-- script-for-nav -->		
+					<div class="search-box">
+				        <form>
+				            <input type="text" name="s" class="textbox" value="search something" onfocus="this.value = '';" onblur="if
+				                  	(this.value == '') {this.value = 'search something';}">
+				            <input type="submit" value>
+				        </form>
+		            </div>
+						<div class="clearfix"> </div>
+				</div>
+			</div>
+	</div>
+<!-- header -->
+<!-- content -->
+	<div class="container">
+		<div class="content">
+			<a href="single.html"><img src="images/img2.jpg" class="img-responsive" alt="" /></a>
+			<i class="aeroplane"> </i>
+	<div class="travelling">
+			<div class="col-md-10 travelling1">
+				<h1><a href="single.html">Traveling to Cilacap Kota Cahaya</a></h1>
+				<h5>by <a href="#">Samidi ,</a> 27 July 2013 ,<a href="#">Traveling</a></h5>
+				<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. </p>
+			</div>
+			<div class="col-md-1 travelling2">
+				<h3>2</h3>
+				<a href="#">comments</a>
+			</div>
+				<div class="clearfix"> </div>
+	</div>
+	<div class="lequotes">
+		
+			<div class="col-md-12">
+				<a class="more" href="single.html">Read More</a>
+			</div>
+			<div class="col-md-12 lequotes3 ">
+				<div class="lequotes1">
+					<img src="images/01.png" class="img-responsive" alt="" />
+				</div>
+				<div class="lequotes2">
+					<h3>Le Quotes</h3>
+				</div>
+					<div class="clearfix"> </div>
+			</div>
+				<div class="billgates">
+				<div class="col-md-10 billgates1">
+					<h2>Quotes from Bill Gates</h2>
+					<h5>by <a href="#">Samidi ,</a> 27 July 2013 ,<a href="#">Quotes</a></h5>
+				</div>
+				<div class="col-md-1 billgates2">
+					<h3>6</h3>
+					<p><a href="#">comments</a></p>
+				</div>
+					<div class="clearfix"> </div>
+					<h4>“Lorem Ipsum dolor sit amet”</h4>
+					<h6>Bill Gates</h6>
+				</div>
+	</div>	
+	<div class="loadmore">
+			<a class="more" href="single.html">Read More</a>
+	</div>
+	<div class="loadmore1">
+			<a class="load" href="#">load More</a>	
+	</div>
+</div>	
+</div>
+<!-- content -->
+<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<p>Template by <a href="http://w3layouts.com" target="_blank"> w3layouts</a></p>
+			<div class="social">
+				<ul>
+					<li><a href="#"><i class="fb"> </i></li></a>
+					<li><a href="#"><i class="twt"> </i></li></a>
+						<div class="clearfix"></div>
+				</ul>
+			</div>
+		</div>
+	</div>
+<!-- footer -->
+</body>
+</html>
